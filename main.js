3c3cb9f71c3f15e2ce1defa6cd58d6b8da50ecb5
Siltaz <16151707+Siltaz@users.noreply.github.com>
2021-02-12 11:08:28 +0530

    main.js fix

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Bot Invite Link: https://discord.com/oauth2/authorize?client_id=807984309906046976&scope=bot&permissions=8
 
 require("dotenv").config();
-const StateManager = require('./utils/StateManager');
+const StateManager = require('./src/utils/StateManager');
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const { registerCommands, registerEvents } = require("./utils/register");
+const { registerCommands, registerEvents } = require("./src/utils/register");
 
 (async () => {
   client.login(process.env.BOT_TOKEN);
